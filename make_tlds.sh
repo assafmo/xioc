482a14c3ca1a2d6c824c954fc520adbaee733160
assafmo <assaf.morami@gmail.com>
2019-01-17 09:29:35 +0200

    tlds.go instead of tlds.json

--- make_tlds.sh
+++ make_tlds.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo -e "package main\n" > tlds.go
+echo "var knownTLDs = []string{" >> tlds.go
+
 curl -s --compressed https://data.iana.org/TLD/tlds-alpha-by-domain.txt |
     grep -vF '#' |
-    awk '{print "\""tolower($0)"\""} END{print "\"onion\""}' |
-    jq -s . > tlds.json
+    awk '{print "\""tolower($0)"\","} END{print "\"onion\","}' >> tlds.go
+
+echo "}" >> tlds.go
